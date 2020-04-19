c5b3e6a42d8358a000ac069082a740ad248dca83
kutuware <39384007+kutuware@users.noreply.github.com>
2020-04-20 00:29:41 +0700

    Initial commit

---
+++ backup/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Moddol</title>
+    <link rel="stylesheet" href="normalize.css">
+</head>
+<body>
+    <style>
+        body {
+            background: rgb(7, 0, 32);
+        }
+    </style>
+    <p>asdasd</p>
+</body>
+</html>
