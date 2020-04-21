f7a025f7f9442ec74de537d0d703075f68a10b1c
kutuware <39384007+kutuware@users.noreply.github.com>
2020-04-21 21:17:27 +0700

    lementer

--- index.js
+++ index.js
@@ -1,56 +1,9 @@
-$(window).on('load', function() {
+$(window).on("load", function () {
   var animation = bodymovin.loadAnimation({
-    container: document.getElementById("fight"),
+    container: document.getElementById("json"),
     renderer: "svg",
     loop: false,
-    autoplay: false,
-    path: "https://assets3.lottiefiles.com/packages/lf20_eHgW4e.json"
-  });
-
-  setTimeout(function () {
-    animation.play();
-  }, 2000);
-
-  AOS.init({
-    offset: -120,
-    delay: 1000,
-    duration: 1200
+    autoplay: true,
+    path: "designinsider.json",
   });
 });
-
-(function ($) {
-  "use strict";
-
-  //Page cursors
-
-  document
-    .getElementsByTagName("body")[0]
-    .addEventListener("mousemove", function (n) {
-      (t.style.left = n.clientX + "px"),
-        (t.style.top = n.clientY + "px"),
-        (e.style.left = n.clientX + "px"),
-        (e.style.top = n.clientY + "px"),
-        (i.style.left = n.clientX + "px"),
-        (i.style.top = n.clientY + "px");
-    });
-  var t = document.getElementById("cursor"),
-    e = document.getElementById("cursor2"),
-    i = document.getElementById("cursor3");
-  function n(t) {
-    e.classList.add("hover"), i.classList.add("hover");
-  }
-  function s(t) {
-    e.classList.remove("hover"), i.classList.remove("hover");
-  }
-  s();
-  for (
-    var r = document.querySelectorAll(".hover-target"), a = r.length - 1;
-    a >= 0;
-    a--
-  ) {
-    o(r[a]);
-  }
-  function o(t) {
-    t.addEventListener("mouseover", n), t.addEventListener("mouseout", s);
-  }
-})(jQuery);
