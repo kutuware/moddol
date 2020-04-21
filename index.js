d7e4f4fe2d69afefe6435452cd033623c681a4c5
kutuware <39384007+kutuware@users.noreply.github.com>
2020-04-22 03:28:53 +0700

    video masking

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ $(window).on("load", function () {
   var animation = bodymovin.loadAnimation({
     container: document.getElementById("json"),
     renderer: "svg",
-    loop: false,
+    loop: true,
     autoplay: true,
     path: "designinsider.json",
   });
